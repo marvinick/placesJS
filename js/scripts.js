9a140191c762815022fba8e97d2db175549d1532
Ian McKenney & marvin nikijuluw <student@epicodus.com>
2015-09-14 12:25:53 -0700

    can't display multiple notes/landmarks

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+$().ready(function() {
+  $("#add-note").click(function() {
+    $("#new-notes").append('<div class="new-note">' +
+                            '<div class="form-group">' +
+                              '<label for="new-note">Note</label>' +
+                              '<input type="text" class="form-control new-note">' +
+                              '</div>' +
+                            '</div>');
+  });
+
+  $("#add-landmark").click(function() {
+    $("#new-landmarks").append('<div class="new-landmark">' +
+                            '<div class="form-group">' +
+                              '<label for="new-landmark">Landmark</label>' +
+                              '<input type="text" class="form-control new-landmark">' +
+                              '</div>' +
+                            '</div>');
+  });
+
+  $("#add-date").click(function() {
+    $("#new-dates").append('<div class="new-date">' +
+                            '<div class="form-group">' +
+                              '<label for="new-from-date">from</label>' +
+                              '<input type="date" class="form-control new-from-date">' +
+                            '</div>' +
+                            '<div class="form-group">' +
+                              '<label for="new-to-date">to</label>' +
+                              '<input type="date" class="form-control new-to-date">' +
+                            '</div>' +
+                          '</div>');
+  });
+
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedPlaceName = $("input#new-place-name").val();
+    var newPlace = { placeName: inputtedPlaceName, notes: [], landmarks: [], dates: [] };
+
+    $(".new-note").each(function() {
+      var inputtedNote = $(this).find("input.new-note").val();
+      newPlace.notes.push(inputtedNote);
+    });
+
+    $(".new-landmark").each(function() {
+      var inputtedLandmark = $(this).find("input.new-landmark").val();
+      newPlace.landmarks.push(inputtedLandmark);
+    });
+
+    $(".new-date").each(function() {
+      var inputtedFromDate = $(this).find("input.new-from-date").val();
+      var inputtedToDate = $(this).find("input.new-to-date").val();
+      var newDate = { fromDate: inputtedFromDate, toDate: inputtedToDate };
+      newPlace.dates.push(newDate);
+    });
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
+
+    $(".place").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeName);
+
+      $("ul#notes").text("");
+      newPlace.notes.forEach(function(note) {
+        $("ul#notes").append("<li>" + note + "</li>");
+      });
+
+      $("ul#landmarks").text("");
+      newPlace.landmarks.forEach(function(landmark) {
+        $("ul#landmarks").append("<li>" + landmark + "</li>");
+      });
+
+      $("ul#dates").text("");
+      newPlace.dates.forEach(function(date) {
+        $("ul#dates").append("<li>" + "from: " + date.fromDate + " to: " +  date.toDate + "</li>");
+      });
+
+      debugger;
+    });
+
+    $("input#new-place-name").val("");
+    $("input.new-landmark").val("");
+    $("input.new-note").val("");
+    $("input.new-from-date").val("");
+    $("input.new-to-date").val("");
+  });
+
+});
